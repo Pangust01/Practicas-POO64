71c2bd94f44d26b017fae445e9ccff30893ead52
Daniel Antonio <danpangust@gmail.com>
2024-04-03 22:57:26 -0500

    update

---
+++ src/modelo/App.java
@@ -0,0 +1,11 @@
+package modelo;
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+    /* 
+    git remote add origin https://github.com/Pangust01/Practicas-POO64.git
+    git branch -M main
+    git push -u origin main
+    */
+}
