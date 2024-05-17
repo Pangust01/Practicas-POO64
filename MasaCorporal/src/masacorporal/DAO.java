cfc022bbbfb5e85ff975e9c701d8a25c3061e03a
Daniel Antonio <153568060+Pangust01@users.noreply.github.com>
2024-05-17 16:00:43 -0500

    Add files via upload

---
+++ MasaCorporal/src/masacorporal/DAO.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package masacorporal;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author ESTUDIANTE
+ */
+public class DAO extends Persona{
+   
+   private List<Persona> personas = new ArrayList<>();
+   
+   public void agregarPersonas(Persona pers){
+       personas.add(pers);
+   }
+   
+   
+   
+   
+   
+}
