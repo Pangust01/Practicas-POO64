cbeb925279e87dd785f5737e6caaacc9c169ad21
Daniel Antonio <153568060+Pangust01@users.noreply.github.com>
2024-06-24 00:02:02 -0500

    Add files via upload

--- CaraFeliz/src/Cara/App.java
+++ CaraFeliz/src/Cara/App.java
@@ -1,15 +1,25 @@
 package Cara;
 
+
 import java.awt.Frame;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 
 public class App {
     public static void main(String[] args) throws Exception {
         Frame ventanaInicial = new Frame();
         CaraFeliz cara = new CaraFeliz();
-        
         ventanaInicial.add(cara);
         ventanaInicial.setSize(400, 400);
         ventanaInicial.setLocationRelativeTo(null);
         ventanaInicial.setVisible(true);
+
+        ventanaInicial.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
     }
 }
--- CaraFeliz/src/Cara/CaraFeliz.java
+++ CaraFeliz/src/Cara/CaraFeliz.java
@@ -6,15 +6,42 @@ import java.awt.Panel;
 
 public class CaraFeliz extends Panel{
     public CaraFeliz(){
-
+        this.setBackground(Color.cyan);
     }
 
     @Override
     public void paint(Graphics g) {
-        // TODO Auto-generated method stub
+
         super.paint(g);
+        //Cara
         g.setColor(Color.yellow);
-        g.fillOval(50, 50, 100, 100);
+        g.fillOval(50, 50, 300, 300);
+        //Ojos
+        g.setColor(Color.black);
+        g.fillOval(110, 120, 70, 70);
+        g.setColor(Color.black);
+        g.fillOval(220, 120, 70, 70);
+        //destellos
+        g.setColor(Color.white);
+        g.fillOval(110, 130, 30, 30);
+        g.fillOval(220, 130, 30, 30);
+        g.fillOval(250, 150, 20, 20);
+        g.fillOval(140, 150, 20, 20);
+
+        //boca
+        g.setColor(Color.DARK_GRAY);
+        g.fillOval(115, 200, 180, 100);
+        g.setColor(Color.yellow);
+        g.fillOval(115, 200, 180, 80);
+        g.setColor(Color.DARK_GRAY);
+        g.fillArc(90, 236, 50, 20, 180, 180);
+        g.fillArc(270, 236, 50, 20, 180, 180);
+        //contorno
+        g.setColor(Color.black);
+        g.drawOval(50, 50, 300, 300);
+
+        
+
     }
 
 }
