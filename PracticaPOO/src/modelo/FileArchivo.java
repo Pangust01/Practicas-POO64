692fa7bc6b6795d86aebbf25aaaa38d5b4a11139
Daniel Antonio <153568060+Pangust01@users.noreply.github.com>
2024-07-15 18:02:30 -0500

    Add files via upload

--- PracticaPOO/src/modelo/FileArchivo.java
+++ PracticaPOO/src/modelo/FileArchivo.java
@@ -4,7 +4,6 @@ import java.awt.Frame;
 import java.awt.TextArea;
 import java.awt.TextField;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.io.Serializable;
 import java.util.Scanner;
@@ -19,11 +18,13 @@ public class FileArchivo implements Serializable{
         jf = new JFileChooser();
     }
 
-    public void escogerDirectorio(TextField txtDirectorio){
+    public void escogerDirectorio(TextField txtDirectorio,TextArea txtPantalla){
         jf.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
         jf.showDialog(null, "Seleccionar");
         txtDirectorio.setText(jf.getSelectedFile().toString());
+        txtPantalla.setText("Se seleccionado un directorio correctamente...");
     }
+
     public void GuardarArchivo(String nomArchivo,TextArea txtPantalla){
         archivo = new File(jf.getSelectedFile(),nomArchivo+".txt");
         try (PrintWriter escribirArchivo = new PrintWriter(archivo)){
@@ -33,6 +34,7 @@ public class FileArchivo implements Serializable{
             e.printStackTrace();
         }
     }
+    
     public void BorrarArchivo(Frame ventana){
         jf.setFileSelectionMode(JFileChooser.FILES_ONLY);
         jf.showOpenDialog(ventana);
@@ -42,9 +44,10 @@ public class FileArchivo implements Serializable{
             archivo.delete();
         }
     }
-    public void LeerArchivo(TextArea txtPantalla){
+    public void LeerArchivo(TextArea txtPantalla, TextField txtDirectorio){
         jf.showOpenDialog(null);
         archivo = jf.getSelectedFile();
+        txtDirectorio.setText(jf.getSelectedFile().toString());
         try (Scanner teclado = new Scanner(archivo)){
             txtPantalla.setText("");
             while (teclado.hasNextLine()) {
--- PracticaPOO/src/vista/Ventana.java
+++ PracticaPOO/src/vista/Ventana.java
@@ -37,9 +37,9 @@ public final class Ventana extends Frame{
         Panel panel5 = new Panel();
         Panel panel6 = new Panel();
         Label lblTitulo = new Label("Bienvenido al sistema para leer y guardar alchivos .txt");
-        Label lblArchivo = new Label("Digame con que nombre quiere que se guarde:");
-        Label lblDirectorio = new Label("Digame donde quiere que se guarde:");
-        TextField txtArchivo = new TextField();
+        Label lblArchivo = new Label("Nombre del archivo:");
+        Label lblDirectorio = new Label("Directorio:");
+        TextField txtArchivo = new TextField(40);
         TextField txtDirectorio = new TextField();
         TextArea txtPantalla = new TextArea("Digame que quiere que contenga el archivo |.txt|");
         Button btnGuardar = new Button("Guardar");
@@ -72,7 +72,7 @@ public final class Ventana extends Frame{
         panel6.add(btnCarpetas);
         archivo = new FileArchivo();
         btnSeleccionar.addActionListener((ActionEvent)->{
-            archivo.escogerDirectorio(txtDirectorio);
+            archivo.escogerDirectorio(txtDirectorio,txtPantalla);
         });
         btnGuardar.addActionListener((ActionEvent)->{
             archivo.GuardarArchivo(txtArchivo.getText(),txtPantalla);
@@ -81,54 +81,74 @@ public final class Ventana extends Frame{
             archivo.BorrarArchivo(this);
         });
         btnLeer.addActionListener((ActionEvent)->{
-            archivo.LeerArchivo(txtPantalla);
+            archivo.LeerArchivo(txtPantalla, txtDirectorio);
+        });
+        btnCarpetas.addActionListener((ActionEvent)->{
+            this.dispose();
+            ventanaCarpetas();
         });
     }
 
-    // public void ventanaCarpetas(){
-    //     Panel panel1 = new Panel();
-    //     Panel panel2 = new Panel(new GridLayout(5,1));
-    //     Panel panel3 = new Panel();
-    //     Panel panel4 = new Panel(new BorderLayout());
-    //     Panel panel5 = new Panel();
-    //     Panel panel6 = new Panel();
-    //     Label lblTitulo = new Label("Bienvenido al sistema para crear y borrar carpetas");
-    //     Label lblDirectorio = new Label("Seleccione una carpeta:");
-    //     TextField txtDirectorio = new TextField();
-    //     TextArea txtPantalla = new TextArea("Aqui... Estara lo que contenga algun archivo seleccionado");
-    //     Button btnGuardar = new Button("Guardar");
-    //     Button btnBorrar = new Button("Borrar");
-    //     Button btnSeleccionar = new Button("Seleccionar");
-    //     Button btnCarpetas = new Button("Cambiar a crear/borrar carpetas");
-    //     this.add(panel1,BorderLayout.NORTH);
-    //     this.add(panel5,BorderLayout.CENTER);
-    //     this.add(panel4,BorderLayout.SOUTH);
-    //     panel1.setBackground(Color.gray);
-    //     panel2.setBackground(Color.gray);
-    //     panel3.setBackground(Color.gray);
-    //     panel4.setBackground(Color.gray);
-    //     panel5.setBackground(Color.gray);
-    //     panel6.setBackground(Color.gray);
-    //     panel5.add(panel2);
-    //     panel5.add(panel3);
-    //     panel1.add(lblTitulo);
-    //     panel2.add(lblDirectorio);
-    //     panel2.add(txtDirectorio);
-    //     panel2.add(btnSeleccionar);
-    //     panel3.add(btnGuardar);
-    //     panel3.add(btnBorrar);
-    //     panel4.add(txtPantalla,BorderLayout.CENTER);
-    //     panel4.add(panel6,BorderLayout.SOUTH);
-    //     panel6.add(btnCarpetas);
-    //     archivo = new FileArchivo();
-    //     btnSeleccionar.addActionListener((ActionEvent)->{
-    //         archivo.escogerDirectorio(txtDirectorio);
-    //     });
-    //     btnGuardar.addActionListener((ActionEvent)->{
+    public void ventanaCarpetas(){
+        Frame ventana2 = new Frame();
+        Panel panel1 = new Panel();
+        Panel panel2 = new Panel(new GridLayout(3,1));
+        Panel panel3 = new Panel();
+        Panel panel4 = new Panel(new BorderLayout());
+        Panel panel5 = new Panel();
+        Panel panel6 = new Panel();
+        Label lblTitulo = new Label("Bienvenido al sistema para crear y borrar carpetas");
+        Label lblDirectorio = new Label("Seleccione una carpeta:");
+        TextField txtDirectorio = new TextField(40);
+        TextArea txtPantalla = new TextArea("Aqui... Dira si hubo algun error o no");
+        Button btnGuardar = new Button("Guardar");
+        Button btnBorrar = new Button("Borrar");
+        Button btnSeleccionar = new Button("Seleccionar");
+        Button btnArchivos = new Button("Cambiar a crear/borrar archivos");
+        ventana2.setLayout(new BorderLayout());
+        ventana2.add(panel1,BorderLayout.NORTH);
+        ventana2.add(panel5,BorderLayout.CENTER);
+        ventana2.add(panel4,BorderLayout.SOUTH);
+        ventana2.setSize(600,400);
+        ventana2.setLocationRelativeTo(null);
+        panel1.setBackground(Color.gray);
+        panel2.setBackground(Color.gray);
+        panel3.setBackground(Color.gray);
+        panel4.setBackground(Color.gray);
+        panel5.setBackground(Color.gray);
+        panel6.setBackground(Color.gray);
+        panel5.add(panel2);
+        panel5.add(panel3);
+        panel1.add(lblTitulo);
+        panel2.add(lblDirectorio);
+        panel2.add(txtDirectorio);
+        panel2.add(btnSeleccionar);
+        panel3.add(btnGuardar);
+        panel3.add(btnBorrar);
+        panel4.add(txtPantalla,BorderLayout.CENTER);
+        panel4.add(panel6,BorderLayout.SOUTH);
+        panel6.add(btnArchivos);
+        archivo = new FileArchivo();
+        btnSeleccionar.addActionListener((ActionEvent)->{
+            archivo.escogerDirectorio(txtDirectorio,txtPantalla);
+
+        });
+        btnGuardar.addActionListener((ActionEvent)->{
            
-    //     });
-    //     btnBorrar.addActionListener((ActionEvent)->{
-    //         archivo.BorrarArchivo(this);
-    //     });
-    // }
+        });
+        btnBorrar.addActionListener((ActionEvent)->{
+            archivo.BorrarArchivo(ventana2);
+        });
+        ventana2.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+        btnArchivos.addActionListener((ActionEvent)->{
+            ventana2.dispose();
+            this.setVisible(true);
+        });
+        ventana2.setVisible(true);
+    }
 }
